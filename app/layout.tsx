6841abff9b9293b81b7ebef15d44f77c19ed8689
Anikwenze Emmanuel <milezkwen@gmail.com>
2024-04-06 12:41:48 +0100

    toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./component/ThemeProvider";
-import { Toaster } from "@/components/ui/sonner";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +24,7 @@ export default function RootLayout({
          defaultTheme="system"
         >
         {children}
-        <Toaster />
+        
         </ThemeProvider>
         </body>
     </html>
